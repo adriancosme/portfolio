7c92272794205a4a28789872d02914c3b1201bcc
Edwin Cosme <adriancosme15@gmail.com>
2023-04-09 10:27:26 -0500

    add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,20 @@
+type HeaderPropsType = {
+    pages: {
+        name: string;
+        to: string;
+    }[];
+};
+
+export const HeaderComponent = ({pages}: HeaderPropsType): JSX.Element => {
+    return <header className="flex justify-center">
+        <nav className="flex gap-2">
+            {
+                pages.map(page => {
+                    return <a className="text-white hover:text-[#4B0ABA]" key={page.name} href={`#${page.to}`}>{page.name}</a>
+                })
+            }
+        </nav>
+    </header>
+}
+
+export default HeaderComponent;
