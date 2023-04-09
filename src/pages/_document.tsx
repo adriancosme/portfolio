1e1049013004ef1194289bbc7d85eaae5f90f5eb
Edwin Cosme <adriancosme15@gmail.com>
2023-04-09 10:37:53 -0500

    add header to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Header from "@/components/header";
+import HeaderPagesData from "../data/header-pages.json";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='bg-[#20222C]'>
+        <Header pages={HeaderPagesData} />
         <Main />
         <NextScript />
       </body>
