710094f1f2c4c1381443aae48a1239eedcff54be
Edwin Cosme <adriancosme15@gmail.com>
2023-04-09 10:37:43 -0500

    fix: remove unused imports from header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Header from "@/components/header";
-import HeaderPagesData from "../data/header-pages.json";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col px-24 py-8 bg-[#20222C]"></main>
