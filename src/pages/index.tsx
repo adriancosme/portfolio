4f7d15453f25335095d3570c054b0b367c0b2290
Edwin Cosme <adriancosme15@gmail.com>
2023-04-09 10:29:12 -0500

    fix: remove header from page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import Header from "@/components/header";
-import HeaderPagesData from '../data/header-pages.json';
+import HeaderPagesData from "../data/header-pages.json";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col px-24 py-8 bg-[#20222C]">
-      <Header pages={HeaderPagesData}></Header>
-    </main>
-  )
+    <main className="flex min-h-screen flex-col px-24 py-8 bg-[#20222C]"></main>
+  );
 }
