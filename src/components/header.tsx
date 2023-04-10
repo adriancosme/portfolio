65694b45521d45c733d0e482252d5af72ddd2e58
Edwin Cosme <adriancosme15@gmail.com>
2023-04-10 15:17:28 -0500

    fix: change anchor element to next link component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,42 @@
+import Link from "next/link";
+
 type HeaderPropsType = {
-    pages: {
-        name: string;
-        to: string;
-    }[];
+  pages: {
+    name: string;
+    to: string;
+  }[];
 };
 
-export const HeaderComponent = ({pages}: HeaderPropsType): JSX.Element => {
-    return <header className="flex justify-center pt-4 bg-[#20222C]">
-        <nav className="flex gap-2">
-            {
-                pages.map(page => {
-                    return <a className="text-white hover:text-[#4B0ABA]" key={page.name} href={`#${page.to}`}>{page.name}</a>
-                })
-            }
-        </nav>
+export const HeaderComponent = ({ pages }: HeaderPropsType): JSX.Element => {
+  return (
+    <header className="flex bg-[#20222C]">
+      <div className="max-w-md mx-auto container lg:max-w-5xl">
+        <section className="flex items-center justify-between min-h-[7.5rem]">
+          <Link href="/">
+            <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+              <span className="font-medium text-gray-600 dark:text-gray-300">
+                EC
+              </span>
+            </div>
+          </Link>
+          <nav className="flex gap-2">
+            {pages.map((page) => {
+              return (
+                <Link
+                  className="text-[#CECECE] hover:text-[#4B0ABA] hover:underline"
+                  key={page.name}
+                  href={`${page.to}`}
+                >
+                  {page.name}
+                </Link>
+              );
+            })}
+          </nav>
+        </section>
+        <section></section>
+      </div>
     </header>
-}
+  );
+};
 
 export default HeaderComponent;
