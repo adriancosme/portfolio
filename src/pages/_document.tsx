9abc1b6f5c860a5de9d31b7ad14d07749623cac0
Edwin Cosme <adriancosme15@gmail.com>
2023-04-11 17:15:25 -0500

    add footer

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 import Header from "@/components/header";
 import HeaderPagesData from "../data/header-pages.json";
 
@@ -6,11 +6,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='bg-[#20222C]'>
+      <body className="bg-[#20222C] scroll-smooth">
         <Header pages={HeaderPagesData} />
         <Main />
         <NextScript />
+        <footer className="bg-[#20222C] w-full flex justify-center">
+          <h2 className="sr-only">Footer</h2>
+          <div className="relative flex items-center pb-2">
+            <span className="text-[#CECECE] text-xs">© 2023 Edwin Cosme. Built with Next.js 13.3.0</span>
+          </div>
+        </footer>
       </body>
     </Html>
-  )
+  );
 }
