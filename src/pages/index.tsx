df0d4617db58c826a48a9c5466774efb33c3181f
Edwin Cosme <adriancosme15@gmail.com>
2023-04-11 17:15:14 -0500

    add about, experience and contact section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,10 @@ import Head from "next/head";
 import Image from "next/image";
 import { SectionComponent } from "@/components/section";
 import Link from "next/link";
+import { MdOutlineArticle } from "react-icons/md";
+import { AiFillGithub, AiOutlineWhatsApp, AiOutlineMail } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
 export default function Home() {
   return (
     <>
@@ -27,7 +31,7 @@ export default function Home() {
               </p>
               <div className="flex gap-5 pt-4">
                 <Link
-                  className="bg-[#1C68B0] text-white p-2.5 rounded-lg text-2xl hover:scale-105 cursor:pointer"
+                  className="bg-[#1C68B0] text-white p-2.5 rounded-lg text-2xl hover:scale-105 cursor-pointer"
                   href={"/"}
                 >
                   Download CV
@@ -55,7 +59,7 @@ export default function Home() {
         </section>
         <SectionComponent id="projects" title="Projects">
           <div className="grid grid-cols-12 gap-5 mt-8">
-            <article className="group col-span-12 lg:col-span-4 border rounded-3xl shadow-md border-gray-700 relative bg-[#CECECE] cursor:pointer">
+            <article className="group col-span-12 lg:col-span-4 border rounded-3xl shadow-md border-gray-700 relative bg-[#CECECE] ">
               <header>
                 <div className="block">
                   <section className="overflow-hidden rounded-t-3xl relative bg-white">
@@ -67,7 +71,7 @@ export default function Home() {
                     />
                   </section>
                 </div>
-                <h3 className="text-[#20222C] px-4 text-lg font-bold ">
+                <h3 className="text-[#20222C] px-4 text-lg font-bold mt-3">
                   Card title
                 </h3>
               </header>
@@ -111,13 +115,93 @@ export default function Home() {
           </div>
         </SectionComponent>
         <SectionComponent id="about-me" title="About me">
-          <p>About me text</p>
+          <p className="mt-8 text-white">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit, lacus tempus
+            laoreet pellentesque ridiculus nam, etiam id gravida hac mi nisl.
+            Lectus scelerisque platea primis auctor consequat a arcu, nisl
+            dictum maecenas duis nulla gravida egestas, pharetra penatibus porta
+            nisi potenti et. Habitant quis fermentum sociis montes libero
+            elementum eget, aptent integer diam feugiat hac ornare velit, in
+            hendrerit augue litora mattis tellus.
+          </p>
         </SectionComponent>
         <SectionComponent id="experience" title="Experience">
-          <p>Exp text</p>
+          <div className="grid grid-cols-12 gap-4 mt-8">
+            <ol className="relative col-span-12">
+              <li className="mb-10 last:mb-0 border-l pl-6">
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-[#1C68B0] rounded-full -left-3">
+                  <MdOutlineArticle color="#fff" size="16" />
+                </span>
+                <div className="flex flex-col justify-between items-start md:flex-row">
+                  <header>
+                    <h3 className="flex items-center mb-1 text-lg font-bold text-white">
+                      Full-stack developer
+                    </h3>
+                    <time className="block mb-2 text-white text-sm font-normal leading-none mt-2">
+                      Aug, 2023 - Aug 2024
+                    </time>
+                  </header>
+                  <Link
+                    href="/"
+                    className="text-white font-bold inline-flex items-center hover:text-[#1C68B0]"
+                  >
+                    31Rooms
+                  </Link>
+                </div>
+                <p className="text-[#CECECE] mt-2">
+                  Lorem ipsum dolor sit amet consectetur adipiscing elit magnis,
+                  nec rutrum sociis condimentum ac accumsan facilisi etiam
+                  sollicitudin
+                </p>
+              </li>
+            </ol>
+          </div>
         </SectionComponent>
         <SectionComponent id="contact" title="Contact me">
-          <p>Contact text</p>
+          <div className="flex flex-col md:flex-row justify-start gap-10">
+            <section className="max-w-sm">
+              <h3 className="text-white mt-4 text-base">
+                Do you have and idea?
+              </h3>
+              <p className="text-white text-base">
+                Send me a message and make it real. You can find me at:
+              </p>
+              <section className="flex mt-6 gap-4">
+                <Link
+                  href="https://github.com/adriancosme"
+                  about="Github profile page"
+                  target="_blank"
+                  className="group flex items-center justify-center w-11 h-11 bg-white hover:bg-[#1C68B0] rounded-full -left-3 cursor-pointer"
+                >
+                  <AiFillGithub className="group-hover:text-white group-hover:scale-110" />
+                </Link>
+                <Link
+                  href="https://www.linkedin.com/in/edwin-cosme/"
+                  about="Linkedin profile page"
+                  target="_blank"
+                  className="group flex items-center justify-center w-11 h-11 bg-white hover:bg-[#1C68B0] rounded-full -left-3 cursor-pointer"
+                >
+                  <FaLinkedinIn className="group-hover:text-white" />
+                </Link>
+                <Link
+                  href="https://wa.me/19841536959"
+                  about="WhatsApp number"
+                  target="_blank"
+                  className="group flex items-center justify-center w-11 h-11 bg-white hover:bg-[#1C68B0] rounded-full -left-3 cursor-pointer"
+                >
+                  <AiOutlineWhatsApp className="group-hover:text-white" />
+                </Link>
+                <Link
+                  href="mailto:adriancosme15@gmail.com"
+                  about="Email address"
+                  target="_blank"
+                  className="group flex items-center justify-center w-11 h-11 bg-white hover:bg-[#1C68B0] rounded-full -left-3 cursor-pointer"
+                >
+                  <AiOutlineMail className="group-hover:text-white" />
+                </Link>
+              </section>
+            </section>
+          </div>
         </SectionComponent>
       </main>
     </>
