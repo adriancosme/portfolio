5dfcf02b471c1443690a855ff7f718a68049803a
Edwin Cosme <adriancosme15@gmail.com>
2023-04-12 13:14:11 -0500

    feat: create experience component

---
+++ src/components/experience.tsx
@@ -0,0 +1,28 @@
+import Job from "./job";
+import { Job as IJob } from "../interfaces/job.interface";
+interface ExperienceProps {
+  data: IJob[];
+}
+const Experience = ({ data }: ExperienceProps) => {
+  return (
+    <div className="grid grid-cols-12 gap-4 mt-8">
+      <ol className="relative col-span-12">
+        {data?.map((job, index) => {
+          return (
+            <Job
+              key={index}
+              position={job.position}
+              period={job.period}
+              company={job.company}
+              websiteCompany={job.websiteCompany}
+              shortDescription={job.shortDescription}
+              archivements={job.archivements}
+            />
+          );
+        })}
+      </ol>
+    </div>
+  );
+};
+
+export default Experience;
