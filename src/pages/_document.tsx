087b253b2776499b3f8401a1a4e712ddf9128740
Edwin Cosme <adriancosme15@gmail.com>
2023-04-20 16:06:20 -0500

    add google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,17 +11,29 @@ export default function Document() {
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTM_ID}`}
         />
+        {/* Google analytics script */}
         <Script
-        id="google-analytics"
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
+          id="google-analytics"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
         
-          gtag('config', '${process.env.GTM_ID}');`
-        }}
-         />
+          gtag('config', '${process.env.GTM_ID}');`,
+          }}
+        />
+        {/* Google Tag Manager script */}
+        <Script
+          id="google-tag-manager"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KMGFKC7');`,
+          }}
+        />
       </Head>
       <body className="bg-[#20222C] scroll-smooth">
         <Header pages={HeaderPagesData} />
@@ -35,6 +47,19 @@ export default function Document() {
             </span>
           </div>
         </footer>
+        {/*<!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KMGFKC7"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
+          ></iframe>
+        </noscript>
+        {/*<!-- End Google Tag Manager (noscript) -->*/}
       </body>
     </Html>
   );
