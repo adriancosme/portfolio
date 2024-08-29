cbf4a9516881c6151e4f45426f6792107b7647c9
Edwin Cosme <adriancosme15@gmail.com>
2024-08-29 14:30:47 -0500

    fix: change atributes style

--- src/components/arrow-link.tsx
+++ src/components/arrow-link.tsx
@@ -54,7 +54,7 @@ export default function ArrowLink({ text, href }: { text: string; href: string }
                     <svg width="60" height="60">
                         <circle stroke="currentColor" strokeWidth="2" fill="transparent" r="28" cx="30" cy="30"></circle>
                         <circle className="text-white" stroke="currentColor" strokeWidth="2" fill="transparent" r="28" cx="30" cy="30"
-                            style={{ strokeDasharray: "175.929px, 175.929px", transform: "rotate(-90deg)", transformOrigin: "30px 30px 0px" }} transform-origin="30px 30px" strokeDashoffset={offset} >
+                            style={{ strokeDasharray: "175.929px, 175.929px", transform: "rotate(-90deg)", transformOrigin: "30px 30px 0px" }} strokeDashoffset={offset} >
                         </circle>
                     </svg>
                 </div>
@@ -64,7 +64,7 @@ export default function ArrowLink({ text, href }: { text: string; href: string }
                         transition: "transform 0.3s ease",
                     }}
                 >
-                    <svg className="transform rotate-0" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M15.101 5.5V23.1094L9.40108 17.4095L8.14807 18.6619L15.9862 26.5L23.852 18.6342L22.5996 17.3817L16.8725 23.1094V5.5H15.101Z" fill="currentColor"></path></svg>
+                    <svg className="transform rotate-0" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M15.101 5.5V23.1094L9.40108 17.4095L8.14807 18.6619L15.9862 26.5L23.852 18.6342L22.5996 17.3817L16.8725 23.1094V5.5H15.101Z" fill="currentColor"></path></svg>
                 </span>
             </div>
             <span className="ml-8 text-xl font-medium">{text}</span>
